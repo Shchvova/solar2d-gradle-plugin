243cf12537f02138d658536440b3a35c66415b22
Vlad Shcherban <vlad@coronalabs.com>
2021-01-14 12:12:13 +0200

    adding basic structure

--- android/buildSrc/build.gradle.kts
+++ android/buildSrc/build.gradle.kts
@@ -4,19 +4,29 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.0.0-alpha04")
+    implementation("com.android.tools.build:gradle:3.5.3")
 }
 
 plugins {
     id("java-gradle-plugin")
+    id("de.fuerstenau.buildconfig") version "1.1.8"
     kotlin("jvm") version "1.3.70"
 }
+
+val pluginId = "com.solar2d.application"
 
 gradlePlugin {
     plugins {
         create("S2DGradlePlugin") {
-            id = "com.solar2d.application"
+            id = pluginId
             implementationClass = "com.solar2d.S2DGP"
         }
     }
+}
+
+buildConfig {
+    buildConfigField("String", "pluginId", pluginId)
+    appName = "Solar2D Android Gradle Plugin"       // sets value of NAME field
+    version = project.version.toString()
+    packageName = "com.solar2d"  // sets the package of the BuildConfig class,
 }
--- android/buildSrc/src/main/kotlin/com/solar2d/S2DGP.kt
+++ android/buildSrc/src/main/kotlin/com/solar2d/S2DGP.kt
@@ -1,25 +1,57 @@
 package com.solar2d
 
+import com.android.build.gradle.AppPlugin
+import com.android.build.gradle.BaseExtension
+import org.gradle.api.InvalidUserCodeException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import com.android.build.gradle.AppPlugin
-import org.gradle.BuildListener
-import org.gradle.BuildResult
-import org.gradle.api.initialization.Settings
-import org.gradle.api.invocation.Gradle
+import org.gradle.api.artifacts.DependencyResolutionListener
+import org.gradle.api.artifacts.ResolvableDependencies
+import org.gradle.api.model.ObjectFactory
+import javax.inject.Inject
 
-class S2DGP : Plugin<Project> {
+abstract class S2DGP : Plugin<Project> {
+
+    @Inject
+    protected abstract fun getObjectFactory(): ObjectFactory?
+
+
+//    project.apply(mapOf("from" to "test.gradle"))
+
+
     override fun apply(project: Project) {
         project.pluginManager.withPlugin("com.android.application") {
-            val androidPlugin = project.plugins.findPlugin(AppPlugin::class.java)!!
-            println(androidPlugin)
-//            val android = androidPlugin.extension
-//            project.afterEvaluate {
-//                android.defaultConfig {
-//                    println("${it.applicationId}")
-//                }
-//            }
-            //androidPlugin.variantManager.createAndroidTasks()
+            val androidPlugin = checkNotNull(project.plugins.findPlugin(AppPlugin::class.java)) { "Unable to find applied Android Application Plugin" }
+            val android = checkNotNull(androidPlugin.extension) { "Unable to find Android Plugin configuration" }
+            project.gradle.addListener(object : DependencyResolutionListener {
+                override fun beforeResolve(dependencies: ResolvableDependencies) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun afterResolve(dependencies: ResolvableDependencies) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+            project.gradle.afterProject {
+                if(it == project) {
+                    configureAndroid(project, android)
+                }
+            }
         }
+        project.afterEvaluate{
+            if(project.plugins.findPlugin(AppPlugin::class.java) == null) {
+                 throw InvalidUserCodeException("The ${BuildConfig.NAME} (\"${BuildConfig.pluginId}\") requires Android Application plugin (\"com.android.application\"), but the project ${project.name} doesn't have it applied.")
+            }
+        }
+    }
+
+    private fun configureAndroid(project: Project, android: BaseExtension) {
+        android.defaultConfig {
+            println(it.applicationId)
+        }
+        project.dependencies( {
+            it.implementation("androidx.multidex:multidex:2.0.1")
+        });
     }
 }
